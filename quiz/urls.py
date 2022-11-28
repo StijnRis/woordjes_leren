fd98c5666ba0f3053a67f8a8c2688baae04c932b
Stijn <131062@stanislascollege.net>
2022-11-28 18:11:48 +0100

    Tutorial

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'quiz'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:list_id>/', views.detail, name='detail'),
+    path('<int:list_id>/words/', views.words, name='results'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
+from quiz.models import List
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the quiz index.")
+    latest_question_list = List.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('quiz/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse("Hello, world. You're at the quiz index.")
+
+def detail(request, list_id):
+    list = get_object_or_404(List, pk=list_id)
+    return render(request, 'quiz/detail.html', {'list': list})
+
+def words(request, list_id):
+    response = "You're looking at the words of question %s."
+    return HttpResponse(response % list_id)
